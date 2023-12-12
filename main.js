f744bd5d76b87d3a6efd4a71ca1a0b3c70a6ab08
baba0049 <144286348+baba0049@users.noreply.github.com>
2023-12-12 14:45:47 -0500

    mad9112 final project

---
+++ main.js
@@ -0,0 +1,6 @@
+function getDaysUntilChristmas()  {
+    const DaysUntilChristmas = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]; 
+    const date = new date(12).getDays(25);
+    return DaysUntilChristmas(date);
+}
+console.log(getDaysUntilChristmas);
